42f583378ff9d363cad1a47ec52aee0b81a02c1f
Marcin Wisnicki <mwisnicki@gmail.com>
2023-08-07 20:48:12 -0400

    Fix test consumers

--- src/test/java/com/example/springkafkatestcontainersdemo/SpringKafkaTestcontainersDemoApplicationTests.java
+++ src/test/java/com/example/springkafkatestcontainersdemo/SpringKafkaTestcontainersDemoApplicationTests.java
@@ -1,6 +1,5 @@
 package com.example.springkafkatestcontainersdemo;
 
-import org.assertj.core.util.IterableUtil;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,13 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.regex.Pattern;
 
 @SpringBootTest
 @Import(TestSpringKafkaTestcontainersDemoApplication.class)
@@ -33,12 +30,8 @@ class SpringKafkaTestcontainersDemoApplicationTests {
     @Test
     void send() throws Exception {
         try (var consumer = consumerFactory.createConsumer("test-send-kafka", null)) {
-            consumer.subscribe(List.of("topic1"));
-            // does not work at all
-            //consumer.subscribe(Pattern.compile(".*"));
-            log.info("Init...");
-            // if next line is commented or duration reduced to less than 1s then receive at the end will fail
-            log.info("Records={}", IterableUtil.toCollection(KafkaTestUtils.getRecords(consumer, Duration.ofSeconds(1))));
+            //consumer.subscribe(List.of("topic1"));
+            consumer.subscribe(Pattern.compile(".*"));
             log.info("Sending message...");
             log.info("Sent: {}", kafkaTemplate.send("topic1", "Hello world " + LocalDateTime.now()).get());
             log.info("Received: {}", KafkaTestUtils.getSingleRecord(consumer, "topic1"));
